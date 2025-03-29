2a90a9c17ddbf93399a0330fd8261ed314e5d7cb
Sarah Mercer <133744854+drsezzer@users.noreply.github.com>
2025-03-29 23:55:44 +0000

    support for loading hexaco loadings.

--- data_support.py
+++ data_support.py
@@ -9,3 +9,32 @@ def fix_name(x):
     :return: 'DaveSmith'
     """
     return x.replace(' ', '').split('.')[0]
+
+
+def load_hexaco_data():
+    """
+    Load the HEXACO data as presente in the 2004 paper.
+    """
+    import pandas as pd
+    loadings = pd.read_csv("hexaco_loadings.csv")
+    loadings.columns = ['Agreeableness', 'a-loadings',
+                        'Extraversion', 'x-loadings',
+                        'Conscientiousness', 'c-loadings',
+                        'Emotionality', 'e-loadings',
+                        'Openness', 'o-loadings',
+                        'Honesty-Humility', 'h-loadings']
+
+    labels = [x for x in loadings.columns if '-load' not in x]
+    loading_labels = [x for x in loadings.columns if '-load' in x]
+
+    data = {}
+    index = []
+
+    for idx, label in enumerate(labels):
+        _weights = loadings[loading_labels[idx]].dropna()
+        _adjs = [x.lower().strip() for x in loadings[labels[idx]].dropna()]
+        _weights.index = [x.lower().strip() for x in _adjs]
+        index += _weights.index.tolist()
+        data[label] = _weights
+
+    return pd.DataFrame(data, index=index)
